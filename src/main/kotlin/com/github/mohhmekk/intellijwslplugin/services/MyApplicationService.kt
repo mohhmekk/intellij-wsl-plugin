1d30afc3cc2f998358047da7ff702c9cc01f9ed9
GitHub Action <action@github.com>
2021-02-10 16:19:28 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mohhmekk/intellijwslplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mohhmekk.intellijwslplugin.services
+
+import com.github.mohhmekk.intellijwslplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/mohhmekk/intellijwslplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mohhmekk/intellijwslplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mohhmekk.intellijwslplugin.services
 
+import com.github.mohhmekk.intellijwslplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
